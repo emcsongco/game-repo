b7f05f0228e0a5856be2f7327ffa198eb7187bc0
Eva S <evamarie.songco@gmail.com>
2022-10-03 06:03:03 -0700

    added grid elements for game

--- index.js
+++ index.js
@@ -25,17 +25,42 @@ const entries = Object.entries(moves);
 const values = Object.values(moves);
 const keys = Object.keys(moves);
 
-console.log(winningCombo);
-console.log(entries);
-console.log(values);
-console.log(keys);
+// console.log(winningCombo);
+// console.log(entries);
+// console.log(values);
+// console.log(keys);
+
+console.log("Grab elements...");
 
 const tiles = document.querySelectorAll(".tiles");
 const start = document.querySelector(".start-button");
 const tile = document.querySelectorAll(".tile");
+
 // console.log(tiles);
 // console.log(tiles.innerHTML);
 // console.log(start);
+
+const t1 = document.getElementById("1-1");
+const t2 = document.getElementById("1-2");
+const t3 = document.getElementById("1-3");
+const t4 = document.getElementById("2-1");
+const t5 = document.getElementById("2-2");
+const t6 = document.getElementById("2-3");
+const t7 = document.getElementById("3-1");
+const t8 = document.getElementById("3-2");
+const t9 = document.getElementById("3-3");
+
+console.log (t1.innerText);
+console.log (t2.innerText);
+console.log (t3.innerText);
+console.log (t4.innerText);
+console.log (t5.innerText);
+console.log (t6.innerText);
+console.log (t7.innerText);
+console.log (t8.innerText);
+console.log (t9.innerText);
+
+console.log("Initialize...");
 
 let lastMove="";
 let movesLeft = 9;
@@ -44,13 +69,17 @@ let p2MovesLeft = 4;
 let token="X";
 let position = "";
 
-console.log ("Game in progress...");
+console.log("Start Game Play");
+
 console.log (`position = ${position}`);
 console.log (`lastMove = ${lastMove}`);
 console.log (`movesLeft = ${movesLeft}`);
 console.log (`p1movesLeft = ${p1MovesLeft}`);
 console.log (`p2MovesLeft = ${p2MovesLeft}`);
 console.log (`token = ${token}`);
+
+console.log ("Game in progress...");
+
 
 tile.forEach(move => {
     move.addEventListener("click", (event) => {
@@ -59,6 +88,7 @@ tile.forEach(move => {
         // console.log (`position = ${position}`);
         // move.innerHTML = token;
         // console.log (`move.innerHTML = ${move.innerHTML}`);
+        // if position == "
         if (movesLeft > 0) {
             move.innerHTML = token;
             if (token === "X") {
